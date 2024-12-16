5f84138a3adad321d05d5c95e3cde751701a8c16
eric hildevert <eric.hildebert@ext.bpifrance.fr>
2024-12-16 10:28:52 +0100

    adding chartjs lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MessageComponent } from './message/message.component';
+import { BarCharttComponent } from './charts/bar-chart.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MessageComponent],
+  imports: [RouterOutlet, MessageComponent, BarCharttComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,21 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { MessageComponent } from './app/message/message.component';
 import { createCustomElement } from '@angular/elements';
+import { BarCharttComponent } from './app/charts/bar-chart.component';
 
 /*bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));*/
 
-  createApplication(appConfig)
+  /*createApplication(appConfig)
   .then((app) => {
       const messageComponent = createCustomElement(MessageComponent, { injector: app.injector });
       customElements.define('mylib-cmp-message', messageComponent);
+  })
+  .catch((err) => console.error(err));*/
+
+  createApplication(appConfig)
+  .then((app) => {
+      const barChartComponent = createCustomElement(BarCharttComponent, { injector: app.injector });
+      customElements.define('mylib-cmp-barchart', barChartComponent);
   })
   .catch((err) => console.error(err));
